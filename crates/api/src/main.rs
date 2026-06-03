3c14107142ae3caebde971b559464da8d5634961
MuyiwaAjiboye <majiboye44@gmail.com>
2026-06-03 13:40:43 +0100

    minor fixes

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -4,7 +4,9 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let config = get_configurations().expect("failded to load configuration file");
+
+    let listener = TcpListener::bind("127.0.0.1:{}, config.application.port")?;
 
     run(listener)?.await
 }
